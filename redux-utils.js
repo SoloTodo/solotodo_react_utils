d61549b616743e0d9229e6cc6f89bebb633b5741
Vijay Khemlani <vkhemlan@gmail.com>
2018-01-17 09:51:34 -0300

    * Moved redux utils to library

---
+++ redux-utils.js
@@ -0,0 +1,69 @@
+import omit from 'lodash/omit';
+
+export function authTokenReducer(state, action) {
+  if (typeof state === 'undefined') {
+    return window.localStorage.getItem('authToken');
+  }
+
+  if (action.type === 'setAuthToken') {
+    if (state !== action.authToken) {
+      if (action.authToken) {
+        window.localStorage.setItem('authToken', action.authToken)
+      } else {
+        window.localStorage.removeItem('authToken')
+      }
+    }
+    return action.authToken
+  }
+
+  return state
+}
+
+export function apiResourceObjectsReducer(state={}, action) {
+  if (action.type === 'addApiResourceObjects' || action.type === 'addApiResource') {
+    let newApiResourceObjects = {};
+    for (const newApiResourceObject of action.apiResourceObjects) {
+      newApiResourceObjects[newApiResourceObject.url] = newApiResourceObject
+    }
+
+    return {...state, ...newApiResourceObjects}
+  }
+
+  if (action.type === 'addApiResourceObject') {
+    return {
+      ...state,
+      [action.apiResource.url]: action.apiResource
+    }
+  }
+
+  if (action.type === 'updateApiResourceObject') {
+    const previousValue = state[action.apiResourceObject.url];
+    const newValue = {...previousValue, ...action.apiResourceObject};
+
+    return {...state, ...{[action.apiResourceObject.url]: newValue}}
+  }
+
+  if (action.type === 'setAuthToken') {
+    // User changed, delete all API resources that include permissions,
+    // this includes the user itself
+    let filteredResources = {...state};
+    Object.values(state)
+        .filter(x => Boolean(x.permissions))
+        .map(x => delete filteredResources[x.url]);
+    return filteredResources
+  }
+
+  if (action.type === 'deleteApiResourceObject') {
+    return omit(state, [action.url])
+  }
+
+  return state
+}
+
+export function loadedResourcesReducer(state=[], action) {
+  if (action.type === 'addApiResource') {
+    return [...state, action.resource]
+  }
+
+  return state
+}
