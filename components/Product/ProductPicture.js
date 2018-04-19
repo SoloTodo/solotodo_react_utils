e85dbb5edb2460b1bb7a9e811c6169694746d152
Vijay Khemlani <vkhemlan@gmail.com>
2018-04-19 15:36:24 -0300

    * Changed ProductPicture style and optimized image loading

--- components/Product/ProductPicture.js
+++ components/Product/ProductPicture.js
@@ -1,23 +1,44 @@
 import React, {Component} from 'react'
 import ImageGallery from 'react-image-gallery';
 import {connect} from "react-redux";
+import './ProductPicture.css';
 
 
 class ProductPicture extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      imageSrc: `${props.product.url}picture/?width=1000&height=520`
+    }
+  }
+
+  handleScreenChange = fullscreenElement => {
+    const imageSrc = fullscreenElement ? this.props.product.pictureUrl :
+        `${this.props.product.url}picture/?width=1000&height=520`;
+
+    this.setState({
+      imageSrc
+    })
+  };
+
   render() {
     const images = [
       {
-        original: this.props.product.pictureUrl,
-        thumbnail: this.props.product.pictureUrl
+        original: this.state.imageSrc,
+        thumbnail: `${this.props.product.url}picture/?width=200&height=200`
       }
     ];
 
     const position = this.props.isMediumOrSmaller ? 'bottom' : 'left';
+    const showThumbnails = images.length > 1;
 
     return <ImageGallery
         showPlayButton={false}
         items={images}
         thumbnailPosition={position}
+        showThumbnails={showThumbnails}
+        onScreenChange={this.handleScreenChange}
     />
   }
 }
