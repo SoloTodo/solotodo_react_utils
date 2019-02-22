bcdc26057b87002eaa2b2f744f624668c44954fb
david <david@solotodo.com>
2019-02-22 09:29:49 -0300

    FIXED: settings endpoints (missing comma)

--- settings.js
+++ settings.js
@@ -30,7 +30,7 @@ export const apiSettings = {
     ratings: endpoint + 'ratings/',
     alerts: endpoint + 'alerts/',
     banner_assets: endpoint + 'banner_assets/',
-    brands: endpoint + 'brands/'
+    brands: endpoint + 'brands/',
     anonymous_alerts: endpoint + 'anonymous_alerts/',
     user_alerts: endpoint + 'user_alerts/',
   },
