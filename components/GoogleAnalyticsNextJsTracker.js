5f12efe932ca98635d84bfc48c14b1f745784c97
Vijay Khemlani <vkhemlan@gmail.com>
2019-02-26 16:58:50 -0300

    * Fixed GoogleAnalyticsNextJsTracker blocking wrapped component getInitialProps

--- components/GoogleAnalyticsNextJsTracker.js
+++ components/GoogleAnalyticsNextJsTracker.js
@@ -2,6 +2,14 @@ import React from 'react';
 
 const withTracker = (WrappedComponent, trackPageHandler) => {
   return class HOC extends React.Component {
+    static async getInitialProps(ctx) {
+      if (WrappedComponent.getInitialProps) {
+        return WrappedComponent.getInitialProps(ctx)
+      }
+
+      return {}
+    }
+
     componentDidMount() {
       trackPageHandler(this.props);
     }
