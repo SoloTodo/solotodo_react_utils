95f5d670c2c2758e5f3243a4c4f3fdcd715c72ff
Vijay Khemlani <vkhemlan@gmail.com>
2019-11-06 15:02:45 -0300

    * Re enabled Hites Lead Link

--- components/LeadLink.js
+++ components/LeadLink.js
@@ -73,9 +73,9 @@ class LeadLink extends React.Component {
     // } else if (store.id === apiSettings.laPolarStoreId) {
     //   url = `https://ad.soicos.com/-16OM?dl=${encodeURIComponent(entity.external_url)}&trackerID=${soicosPrefix || ''}${entity.active_registry.id}${urlSuffix}`;
     //   target = '_self'
-    // } else if (store.id === apiSettings.hitesStoreId) {
-    //   url = `https://ad.soicos.com/-16ON?dl=${encodeURIComponent(entity.external_url)}&trackerID=${soicosPrefix || ''}${entity.active_registry.id}${urlSuffix}`;
-    //   target = '_self'
+    } else if (store.id === apiSettings.hitesStoreId) {
+      url = `https://ad.soicos.com/-16ON?dl=${encodeURIComponent(entity.external_url)}&trackerID=${soicosPrefix || ''}${entity.active_registry.id}${urlSuffix}`;
+      target = '_self'
     } else if (store.id === apiSettings.hpOnlineStoreId) {
       url = `https://www.awin1.com/cread.php?awinmid=15305&awinaffid=641001&clickref=&p=%5B%5B${encodeURIComponent(entity.external_url)}%5D%5D`;
       target = '_self'
