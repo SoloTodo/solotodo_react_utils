747507abc6d0c44dccb034fc16925bffc945cd2d
Vijay Khemlani <vkhemlan@gmail.com>
2020-11-26 09:39:43 -0300

    * Updated LeadLink

--- components/LeadLink.js
+++ components/LeadLink.js
@@ -85,7 +85,7 @@ class LeadLink extends React.Component {
       url = `https://www.awin1.com/cread.php?awinmid=15305&awinaffid=641001&clickref=&p=%5B%5B${encodeURIComponent(entity.external_url)}%5D%5D`;
       target = '_self'
      } else if (store.id === apiSettings.falabellaStoreId) {
-       url = `https://ad.soicos.com/-14Zg?dl=${encodeURIComponent(entity.external_url)}&trackerID=${soicosPrefix || ''}${entity.active_registry.id}${urlSuffix}`;
+       url = `https://ad.soicos.com/-1duC?dl=${encodeURIComponent(entity.external_url)}&trackerID=${soicosPrefix || ''}${entity.active_registry.id}${urlSuffix}`;
        target = '_self'
     } else if (store.id === apiSettings.huaweiShopStoreId) {
       url = `https://ad.soicos.com/-1cEy?dl=${encodeURIComponent(entity.external_url)}&trackerID=${soicosPrefix || ''}${entity.active_registry.id}${urlSuffix}`;
