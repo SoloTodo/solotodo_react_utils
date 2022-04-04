a2b17ca8ad681d826bd9adfb13c1dfe95dd72509
Vijay Khemlani <vj@solotodo.com>
2022-04-04 14:33:32 -0400

    * Changed LeadLink Hites rules

--- components/LeadLink.js
+++ components/LeadLink.js
@@ -92,9 +92,9 @@ class LeadLink extends React.Component {
     } else if (store.id === apiSettings.reuseStoreId) {
       url = `https://ad.soicos.com/-1i2E?dl=${encodeURIComponent(entity.external_url)}&trackerID=${soicosPrefix || ''}${entity.active_registry.id}${urlSuffix}`;
       target = '_top'
-    } else if (store.id === apiSettings.hitesStoreId) {
-      url = `https://ad.soicos.com/-16ON?dl=${encodeURIComponent('https://www.hites.com/')}&trackerID=${soicosPrefix || ''}${entity.active_registry.id}${urlSuffix}`;
-      target = '_top'
+    // } else if (store.id === apiSettings.hitesStoreId) {
+    //   url = `https://ad.soicos.com/-16ON?dl=${encodeURIComponent('https://www.hites.com/')}&trackerID=${soicosPrefix || ''}${entity.active_registry.id}${urlSuffix}`;
+    //   target = '_top'
     } else if (store.id === apiSettings.hpOnlineStoreId) {
       url = `https://www.awin1.com/cread.php?awinmid=15305&awinaffid=641001&clickref=&p=%5B%5B${encodeURIComponent(entity.external_url)}%5D%5D`;
       target = '_self'
